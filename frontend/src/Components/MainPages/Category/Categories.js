58d7637a236b5f8c0093f45f3b26e9b400bb33e4
DilkiThakshmila <dilkithakshmila@gmail.com>
2022-12-17 11:18:02 +0530

    category features changes

--- frontend/src/Components/MainPages/Category/Categories.js
+++ frontend/src/Components/MainPages/Category/Categories.js
@@ -14,25 +14,29 @@ export default function Categories() {
             <h1>Unbeleivable Solutions<br/> For All Legel Cases </h1><br/>
             <h3>We approach each problem with three essential elements:<br/><u>Strategic thinking</u><br/><u>Creative solusions</u><br/><u>Contacting lawyers</u></h3>
           </div>
-          <section id="features">
-          <div className="box1"></div>
-          <div className="box2"></div>
-          <div className="box3"></div>
-          <div className="box4"></div>
+          
+<section class="intro-area white" id="intro">
+  <div class="row">
+	<div class="col-sm-4">
+		<div class="intro-block">
+			<h3>Car Accident</h3>
 
-          <div className="text-on-box1">
-            <h3><i><b>Car Accident</b></i></h3>
+	</div>
+</div>
+
+<div class="col-sm-4">
+		<div class="intro-block">
+			<h3>Child Welfare</h3>
+	      </div>
+             </div>
+
+<div class="col-sm-4">
+		<div class="intro-block">
+			<h3>Family Laws</h3>
+	     </div>
           </div>
-          <div className="text-on-box2">
-            <h3><i><b>Child Welfare</b></i></h3>
           </div>
-          <div className="text-on-box3">
-            <h3><i><b>Family Law</b></i></h3>
-          </div>
-          <div className="text-on-box4">
-            <h3><i><b>Family Law</b></i></h3>
-          </div>
-          </section>
+</section>
          <div>
            <div>
            <li className="header.font"><Link to="/login" >SignIn</Link></li>
