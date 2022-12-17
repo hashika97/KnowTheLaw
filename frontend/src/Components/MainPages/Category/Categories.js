5f60584f4db52f573cf9b1356f890117eb3f124d
DilkiThakshmila <dilkithakshmila@gmail.com>
2022-12-17 10:44:10 +0530

    adding category features

--- frontend/src/Components/MainPages/Category/Categories.js
+++ frontend/src/Components/MainPages/Category/Categories.js
@@ -12,15 +12,27 @@ export default function Categories() {
           </div>
           <div className="text-on-image">
             <h1>Unbeleivable Solutions<br/> For All Legel Cases </h1><br/>
-            <h3>A best experience<br/>easy to read and easy to understand about the basics of laws</h3>
+            <h3>We approach each problem with three essential elements:<br/><u>Strategic thinking</u><br/><u>Creative solusions</u><br/><u>Contacting lawyers</u></h3>
           </div>
-          
+          <section id="features">
           <div className="box1"></div>
           <div className="box2"></div>
-          <div className="text-on-box">
-            <h3><i>Car Accident</i><br/><br/><i>Child Welfare</i><br/><br/><i>Family Law</i></h3>
-          </div>
+          <div className="box3"></div>
+          <div className="box4"></div>
 
+          <div className="text-on-box1">
+            <h3><i><b>Car Accident</b></i></h3>
+          </div>
+          <div className="text-on-box2">
+            <h3><i><b>Child Welfare</b></i></h3>
+          </div>
+          <div className="text-on-box3">
+            <h3><i><b>Family Law</b></i></h3>
+          </div>
+          <div className="text-on-box4">
+            <h3><i><b>Family Law</b></i></h3>
+          </div>
+          </section>
          <div>
            <div>
            <li className="header.font"><Link to="/login" >SignIn</Link></li>
